a884dc63a6408f746ddcc9a7870c41dde029995e
tushari789 <tushar789imran@gmail.com>
2023-07-22 03:30:59 +0600

    navbar, login page design and darkmode and light mode done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Registration from './Pages/Registration/Registration'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Registration/>
+    </div>
+  )
+}
+
+export default App
