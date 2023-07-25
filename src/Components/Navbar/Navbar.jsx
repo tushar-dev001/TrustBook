9bff5b0f2b8408bb50ec4b24cc31afd73dfed8b1
tushari789 <tushar789imran@gmail.com>
2023-07-26 00:07:06 +0600

    lgoin and register button click problem solved

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,23 +1,25 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import DarkMode from '../DarkMode/DarkMode'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import DarkMode from "../DarkMode/DarkMode";
 
 const Navbar = () => {
-
   return (
     <div className="navbar bg-primary text-white">
-  <div className="flex-1">
-    <button className="btn btn-ghost normal-case text-xl">TrustBook</button>
-  </div>
-  <div className="flex-none p-2 pr-5 gap-4">
-    
-    <button className='btn btn-secondary text-white'><Link to='/login'>Login</Link></button>
-    <button className='btn btn-secondary text-white'><Link to='/'>Register</Link></button>
+      <div className="flex-1">
+        <button className="btn btn-ghost normal-case text-xl">TrustBook</button>
+      </div>
+      <div className="flex-none p-2 pr-5 gap-4">
+        <Link to="/login">
+          <button className="btn btn-secondary text-white">Login</button>
+        </Link>
+        <Link to="/">
+          <button className="btn btn-secondary text-white">Register</button>
+        </Link>
 
-  <DarkMode/>
-  </div>
-</div>
-  )
-}
+        <DarkMode />
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
